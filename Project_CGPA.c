8984aee9ae84b4f6f98d116323d33849a2a5481a
Akif Zahir <124380580+akifzahir7175@users.noreply.github.com>
2025-07-10 19:04:02 +0600

    Update Project_CGPA.c

--- Project_CGPA.c
+++ Project_CGPA.c
@@ -9,6 +9,7 @@ int main() {
     int sem,course;
     printf("Enter number of semester: ");
     scanf("%d", &sem);
+    
 
      for (int i = 1; i <= sem; i++){
          printf("_________________________________________________________________________\n");
@@ -25,18 +26,24 @@ int sgpa(){
     scanf("%d", &course);
     
     float arr[2][course];
-    printf("Course name       Course Credit      Obtained grade       ");
-    for (int i = 0; i < course; i++){
 
+    for (int i = 0; i < course; i++){
+        
+        printf("Course %d name: ", i);
         scanf("%c", &x);
+        
+        printf("Course Credit: ");
         scanf("%f", &arr[0][i]);
         a = a + arr [0][i];
+        
+        printf("Obtained Grade: ");
         scanf("%f", &arr[1][i]);
         b = b + arr[0][i];
+        
         c = c + arr [0][i] * arr [1][i];
      }
      d = c/a;
-     printf("\nTotal        %.1f        %.2f\n", a, d);
+     printf("\nTotal Credit this Semester: %.1f\nSGPA: %.2f\n", a, d);
 }
 
 
